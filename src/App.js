1e68b61aa87e873f945090f98a2c8ccde4356958
Ninaad <ninaadps@gmail.com>
2022-10-19 20:11:40 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Card, Button } from "antd";
+import axios from "axios";
+import "./App.css";
+
+const { Meta } = Card;
+
 
 function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const loadNews = async () => {
+      const response = await axios.get(
+        "http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=3da128da75bb4e819bb876090635ca8f"
+      );
+      setNews(response.data.articles);
+    };
+    loadNews();
+  }, []);
+
+  console.log("news", news);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          
+      {news &&
+        news.map((item, index) => {
+          return (
+            <Card
+              key={index}
+              hoverable
+              style={{ width: "70%" }}
+              cover={<img alt="image" src={item.urlToImage} />}
+            >
+              <Meta title={item.title} description={item.content} />
+              <a href={item.url} target="_blank" rel="noopener noreferrer">
+                <Button type="primary" style={{ marginTop: "10px" }}>
+                  Read More
+                </Button>
+              </a>
+            </Card>
+          );
+        })}
     </div>
   );
 }
